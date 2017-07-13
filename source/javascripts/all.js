240f4a720bf3846b47b69d10d13fce04ff8148cf
勝田圭豊 <kkrt0724@gmail.com>
2017-07-13 14:45:11 +0900

    指摘箇所修正

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,16 +1,16 @@
-// $(function(){
-//   $('.tab li').click(function(){
-//     var index = $('.tab li').index(this);
-//     $('.content li').hide();
-//     $('.content li').eq(index).fadeIn();
-//     $('.tab li').removeClass('select');
-//     $(this).addClass('select')
-//   });
-// });
+$(function(){
+  $('.tab li').click(function(){
+    var index = $('.tab li').index(this);
+    $('.content li').hide();
+    $('.content li').eq(index).fadeIn();
+    $('.tab li').removeClass('select');
+    $(this).addClass('select')
+  });
+});
 
 
 $(function(){
-        $("#acMenu dt").on("click", function() {
-            $(this).next().slideToggle();
-        });
-    });
+  $("#acMenu dt").on("click", function() {
+    $(this).next().slideToggle();
+  });
+});
