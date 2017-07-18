1ebc4dd4fc4bcf121c3fa8429db5f26afe54ce53
勝田圭豊 <kkrt0724@gmail.com>
2017-07-18 17:13:58 +0900

    json to html

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,16 +1,36 @@
-$(function(){
-  $('.tab li').click(function(){
-    var index = $('.tab li').index(this);
-    $('.content li').hide();
-    $('.content li').eq(index).fadeIn();
-    $('.tab li').removeClass('select');
-    $(this).addClass('select')
-  });
-});
+// $(function(){
+//   $('.tab li').click(function(){
+//     var index = $('.tab li').index(this);
+//     $('.content li').hide();
+//     $('.content li').eq(index).fadeIn();
+//     $('.tab li').removeClass('select');
+//     $(this).addClass('select')
+//   });
+// });
+//
+//
+// $(function(){
+//   $("#acMenu dt").on("click", function() {
+//     $(this).next().slideToggle();
+//   });
+// });
 
 
+// ---------json--------------
 $(function(){
-  $("#acMenu dt").on("click", function() {
-    $(this).next().slideToggle();
+  $.ajax({
+    method:"GET"
+    url :"members.json"
+    dataType:"json"
+  }).done(function(data){
+    for(var i=0;i<data.length;i++){
+      var tags = "";
+      for(var dt in deta[i]){
+        tags = tags+"<td>"+ data.name[i][dt]+"</td>";
+        tags = tags+"<td>"+ data.jp_name[i][dt]+"</td>";
+        tags = tags+"<td>"+ data.id[i][dt]+"</td>";
+      }
+      $("#tbl").append("<tr>" + tags + "</tr>");
+    }
   });
 });
